23f65d1314fe2da127ebef487f05514eb39eba73
Peter <peter.rawsthorne@oceanset.com>
2023-09-12 14:22:11 -0230

    used npm

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Hello World!")
+const http = require('http');
+// const hostname = '127.0.0.1';
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((request, response) => {
+  console.log(request);
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hello Keyin World on a cloudy day.');
+});
+
+console.log("It is cloudy today.")
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
